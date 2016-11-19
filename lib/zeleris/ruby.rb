7d405ac0dc09b62096bafe7fdc9faedb818d8ef0
alexperezpaya <alejandro@perezpaya.net>
2016-11-20 00:59:30 +0100

    Gem init

---
+++ lib/zeleris/ruby.rb
@@ -0,0 +1,7 @@
+require "zeleris/ruby/version"
+
+module Zeleris
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/zeleris/ruby/version.rb
@@ -0,0 +1,5 @@
+module Zeleris
+  module Ruby
+    VERSION = "0.1.0"
+  end
+end
